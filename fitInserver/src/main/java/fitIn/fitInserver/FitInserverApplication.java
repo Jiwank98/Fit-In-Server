f53ce7ffe7b430d8ae95c06bcd49f13761ddbdd9
js819park <js819park@gmail.com>
2022-02-07 22:27:15 +0900

    HDS-2

---
+++ fitInserver/src/main/java/fitIn/fitInserver/FitInserverApplication.java
@@ -0,0 +1,13 @@
+package fitIn.fitInserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitInserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitInserverApplication.class, args);
+	}
+
+}
